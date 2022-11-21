865a55b5b2447d7c64b155aef9ba63ab6df2f607
韦杨婧 <981193371@qq.com>
2022-11-22 03:28:14 +0800

    改写成类

--- code/tryFlask.py
+++ code/tryFlask.py
@@ -5,12 +5,15 @@ from flask import (
     Flask, render_template, request, redirect, url_for)
 import control 
 import json
-# import multiprocessing
+import multiprocessing
+from multiprocessing import Process
 import random
+from werkzeug.local import Local
 
 search = Flask(__name__)
 
-class g:
+class gVal:
+
     # register--0|login--1|info--2
     status = 1
     # login error
@@ -29,320 +32,390 @@ class g:
     starttime = 0
     # 是否进行过测试
     anytest = 0
-    # 引入control的函数
-    ctrl=control.control()
     # 选择的内容
     choice = 0
 
-globals=g()
+    # 引入control的函数
+    ctrl=control.control()
+
+class gFunc:
+    def __init__(self):
+        self.g = gVal()
+        self.g.ctrl=control.control()
+
+    def index(self):
+        self.g = gVal()
+        temp=random.randint(0, 200)
+        detail=self.g.ctrl.get_book_detail_info(temp+1)
+        return render_template(
+            'index.html',
+            status=self.g.status,
+            detail=detail,
+            bookReID=temp
+            )
+        
+    def index1(self):
+        temp=random.randint(0, 200)
+        detail=self.g.ctrl.get_book_detail_info(temp+1)
+        return render_template(
+            'index.html',
+            status=self.g.status,
+            detail=detail,
+            bookReID=temp
+            )
+
+    def books(self):
+        books=self.g.ctrl.get_book_brief_info()
+        return render_template(
+            'books.html',
+            status=self.g.status,
+            book=books
+        )
+    
+    def bookDetail(self, id):
+        detail=self.g.ctrl.get_book_detail_info(id+1)
+        if (detail[16] or detail[17]):
+            foreign=1
+        else:
+            foreign=0
+        return render_template(
+            "bookDetail.html",
+            detail=detail,
+            foreign=foreign,
+            status=self.g.status,
+            i=id
+        )
+    
+    #here
+    def test(self): 
+        if self.g.status == 1:
+            self.g.test=1
+            self.g.stat=0
+            return render_template(
+                "profile.html",
+                status=self.g.status
+            )
+        if self.g.status == 2:
+            return render_template(
+                'test.html',
+                status=self.g.status,
+                )
+            
+    def testID(self, id):
+        whole=0
+        head=['A','B','C','D']
+        self.g.ques=self.g.ctrl.get_question_by_bookid(id)
+        self.g.starttime=int(time.time())
+        return render_template(
+            "testStart.html",
+            status=self.g.status,
+            test=self.g.ques,
+            head=head,
+            whole=whole,
+            length=len(self.g.ques)
+    )
+        
+    def testSelected(self):
+        data=json.loads(list(request.args.to_dict().keys())[0])
+        testType=data["selected"][0]
+        if testType=="random":                                          #随机出题测试
+            test=self.g.ctrl.get_question_random()
+        else:                                                           #指定类型随机出题测试
+            test=self.g.ctrl.get_question_by_category(testType)
+        self.g.ques=test
+        self.g.starttime=int(time.time())
+        return redirect(url_for('index1'))
+    
+    def testStart(self):
+        whole=0
+        head=['A','B','C','D']
+        return render_template(
+            "testStart.html",
+            status=self.g.status,
+            test=self.g.ques,
+            head=head,
+            whole=whole,
+            length=len(self.g.ques)
+        )
+        
+    def submit(self):
+    
+        returnData=json.loads(list(request.args.to_dict().keys())[0])           # {'titleID_choice': ['20_true']}
+        choice=returnData["titleID_choice"]                                     # ['2_true', '5_true', '7_true', '9_芝麻,开门吧!', '20_true', '21_鲁贵', '24_true'] 0--24
+        self.g.choice=dict()
+        for item in choice:
+            self.g.choice[item.split("_")[0]]=item.split("_")[1]
+        
+        test=self.g.ques
+        returnData.update({'test':test})
+        returnData.update({"userid":self.g.User["id"]})
+
+        endtime=int(time.time())
+        returnData.update({"starttime":self.g.starttime})
+        returnData.update({"endtime":endtime})
+        returnData.update({"questionnum":len(self.g.ques)})
+        self.g.ctrl.check(returnData)
+
+        return redirect(url_for('index1'))
+    
+    def process(self):
+        whole=0
+        head=['A','B','C','D']
+        return render_template(
+            "process.html",
+            status=self.g.status,
+            whole=whole,
+            head=head,
+            length=len(self.g.ques),
+            test=self.g.ques,
+            choice=self.g.choice
+        )
+        
+    def statistics(self):
+        self.g.anytest=0
+        self.g.stat = 1
+        self.g.test = 0
+        if self.g.status == 1:
+            return render_template(
+                "profile.html",
+                status=self.g.status
+            )
+        if self.g.status == 2:
+            data = self.g.ctrl.get_user_statistics(int(self.g.User["id"]))
+            if data == None:
+                self.g.anytest = 1
+                return render_template(
+                    "statistics.html",
+                    status=self.g.status,
+                    anytest=self.g.anytest
+                )
+            else:
+                return render_template(
+                        'statistics.html',
+                        status=self.g.status,
+                        username=self.g.User["name"],
+                        times=data["count"],
+                        avgDuration=str(data["avgduration"]//60)+"分"+str(data["avgduration"]%60)+"秒",
+                        avgAccuracy=int(data["avgscore"]),
+                        lastDuration=str(data["lastduration"]//60)+"分"+str(data["lastduration"]%60)+"秒",
+                        lastAccuracy=int(data["lastscore"]),
+                        lastTime=time.strftime("%Y年%m月%d日 %H时%M分%S秒",time.localtime(data["lasttime"])),
+                        anytest=self.g.anytest,
+                        Testscore=data["scores"]
+                    )
+    
+    def profile(self):
+        return render_template(
+            'profile.html',
+            status=self.g.status,
+            id=self.g.User["id"],
+            name=self.g.User["name"],
+            gender=self.g.User["gender"],
+            tele=self.g.User["tele"],
+            brief=self.g.User["brief"]
+        )
+        
+    def login(self):
+        self.g.status = 1
+        self.g.test = 0
+        self.g.stat = 0
+        return render_template(
+            'profile.html',
+            status=self.g.status,
+            error=self.g.error,
+            retry=self.g.retry
+            )
+        
+    def register(self):
+        self.g.status=0
+        return render_template(
+            "profile.html",
+            status=self.g.status,
+            error=self.g.error,
+            retry=self.g.retry
+            )
+        
+    def registration(self):
+        self.g.error=0
+        self.g.retry=0
+        pw=request.args.get('pwReg')
+        pw2=request.args.get('pw2Reg')
+        if pw==pw2:
+            name=request.args.get("nameReg")
+            gender=request.args.get("genderReg")
+            tele=request.args.get("teleReg")
+            brief=request.args.get("briefReg")
+            if(name and gender and tele and brief and pw and pw2):
+                id=self.g.ctrl.new_user(name,gender,tele,pw,brief)
+                self.g.status=1
+                return render_template(
+                    "validation.html",
+                    status=self.g.status,
+                    id=id
+                )
+            else:
+                self.g.retry=2
+                return render_template(
+                    "profile.html",
+                    status=self.g.status,
+                    retry=self.g.retry
+                )
+        else:
+            self.g.retry=1
+            return render_template(
+                "profile.html",
+                status=self.g.status,
+                retry=self.g.retry
+            )
+    
+    def validation(self):
+        self.g.error=0
+        self.g.retry=0
+        pw=str(request.form.get('pw'))
+        id=request.form.get('id')
+        temp=self.g.ctrl.get_user_info_by_id(id)
+        try:
+            # 是否输对密码
+            if temp[4]==pw:
+                self.g.User={"id":temp[0],"name":temp[1],"gender":temp[2],"tele":temp[3],"pw":temp[4],"brief":temp[5]}
+                self.g.status = 2
+
+                # 如果从test栏进入，返回test界面
+                if self.g.test == 1:
+                    self.g.test = 0
+                    return redirect(url_for('test'))
+                
+                # 如果从stat栏进入，返回stat界面
+                elif self.g.stat == 1:
+                    self.g.stat = 0
+                    return redirect(url_for('statistics'))
+                
+                # 否则返回首页
+                else:
+                    try:
+                        return redirect(url_for('index1'))
+                    except:
+                        return redirect(url_for('login'))
+
+            # 输错密码
+            else:
+                print(2)
+                self.g.error=1
+                return render_template(
+                    "profile.html",
+                    status=self.g.status,
+                    error=self.g.error
+                    )
+        # 没输入密码
+        except:
+            print(3)
+            self.g.error=1
+            return render_template(
+                "profile.html",
+                status=self.g.status,
+                error=self.g.error
+                )
+    
+    def logout(self):
+        self.g.status=1
+        return redirect(url_for('index1'))
+
+globals=gVal()
+globalsF=gFunc()
 
 # 主页
 @search.route('/', methods=['GET', 'POST'])
 def index():
-    temp=random.randint(0, 200)
-    detail=globals.ctrl.get_book_detail_info(temp+1)
-    return render_template(
-        'index.html',
-        status=globals.status,
-        detail=detail,
-        bookReID=temp
-        )
+    return globalsF.index()
+
+# 伪首页
+@search.route('/index', methods=['GET', 'POST'])
+def index1():
+    return globalsF.index1()
 
 # 书籍概览    
 @search.route('/books', methods=['GET', 'POST'])
 def books():
-    books=globals.ctrl.get_book_brief_info()
-    return render_template(
-        'books.html',
-        status=globals.status,
-        book=books
-    )
+    return globalsF.books()
 
 # 书籍详情页
 @search.route('/bookDetail/<int:id>', methods=['GET', 'POST'])
 def bookDetail(id):
-    detail=globals.ctrl.get_book_detail_info(id+1)
-    if (detail[16] or detail[17]):
-        foreign=1
-    else:
-        foreign=0
-    return render_template(
-        "bookDetail.html",
-        detail=detail,
-        foreign=foreign,
-        status=globals.status,
-        i=id
-    )
+    return globalsF.bookDetail(id)
 
 # 测试界面
 @search.route('/test', methods=['GET', 'POST'])
 def test(): 
-    if globals.status == 1:
-        globals.test=1
-        globals.stat=0
-        return render_template(
-            "profile.html",
-            status=globals.status
-        )
-    if globals.status == 2:
-        return render_template(
-            'test.html',
-            status=globals.status,
-            )
+    return globalsF.test()
 
 # 书籍详情页进入测试        
 @search.route('/testID/<int:id>',methods=['GET', 'POST'])
 def testID(id):
-    whole=0
-    head=['A','B','C','D']
-    globals.ques=globals.ctrl.get_question_by_bookid(id)
-    globals.starttime=int(time.time())
-    return render_template(
-        "testStart.html",
-        status=globals.status,
-        test=globals.ques,
-        head=head,
-        whole=whole,
-        length=len(globals.ques)
-    )
+    return globalsF.testID(id)
 
 # 选取测试题目的类型
 @search.route('/testSelected', methods=['GET', 'POST'])
 def testSelected():
-    data=json.loads(list(request.args.to_dict().keys())[0])
-    testType=data["selected"][0]
-    if testType=="random":                                          #随机出题测试
-        test=globals.ctrl.get_question_random()
-    else:                                                           #指定类型随机出题测试
-        test=globals.ctrl.get_question_by_category(testType)
-    globals.ques=test
-    globals.starttime=int(time.time())
-    return render_template(
-        "index.html",
-        status=globals.status
-    )
+    return globalsF.testSelected()
  
 # 正式测试
 @search.route('/testStart', methods=['GET', 'POST'])
 def testStart():
-    whole=0
-    head=['A','B','C','D']
-    return render_template(
-        "testStart.html",
-        status=globals.status,
-        test=globals.ques,
-        head=head,
-        whole=whole,
-        length=len(globals.ques)
-    )
+    return globalsF.testStart()
 
 # 提交
 @search.route('/submit', methods=['GET', 'POST'])
 def submit():
-
-    returnData=json.loads(list(request.args.to_dict().keys())[0])           # {'titleID_choice': ['20_true']}
-    choice=returnData["titleID_choice"]                                     # ['2_true', '5_true', '7_true', '9_芝麻,开门吧!', '20_true', '21_鲁贵', '24_true'] 0--24
-    globals.choice=dict()
-    for item in choice:
-        globals.choice[item.split("_")[0]]=item.split("_")[1]
-    
-    test=globals.ques
-    returnData.update({'test':test})
-    returnData.update({"userid":globals.User["id"]})
-
-    endtime=int(time.time())
-    returnData.update({"starttime":globals.starttime})
-    returnData.update({"endtime":endtime})
-    returnData.update({"questionnum":len(globals.ques)})
-    globals.ctrl.check(returnData)
-
-    return render_template(
-        "index.html",
-        status=globals.status,
-        test=test,
-        choice=choice
-    )
+    return globalsF.submit()
 
 # 处理数据
 @search.route('/process', methods=['GET', 'POST'])
 def process():
-    whole=0
-    head=['A','B','C','D']
-    return render_template(
-        "process.html",
-        status=globals.status,
-        whole=whole,
-        head=head,
-        length=len(globals.ques),
-        test=globals.ques,
-        choice=globals.choice
-    )
+    return globalsF.process()
 
 # 数据分析
 @search.route('/statistics', methods=['GET', 'POST'])
 def statistics():
-    globals.anytest=0
-    globals.stat = 1
-    globals.test = 0
-    if globals.status == 1:
-        return render_template(
-            "profile.html",
-            status=globals.status
-        )
-    if globals.status == 2:
-        data = globals.ctrl.get_user_statistics(int(globals.User["id"]))
-        if data == None:
-            globals.anytest = 1
-            return render_template(
-                "statistics.html",
-                status=globals.status,
-                anytest=globals.anytest
-            )
-        else:
-            return render_template(
-                'statistics.html',
-                status=globals.status,
-                username=globals.User["name"],
-                times=data["count"],
-                avgDuration=str(data["avgduration"]//60)+"分"+str(data["avgduration"]%60)+"秒",
-                avgAccuracy=int(data["avgscore"]),
-                lastDuration=str(data["lastduration"]//60)+"分"+str(data["lastduration"]%60)+"秒",
-                lastAccuracy=int(data["lastscore"]),
-                lastTime=time.strftime("%Y年%m月%d日 %H时%M分%S秒",time.localtime(data["lasttime"])),
-                anytest=globals.anytest,
-                Testscore=data["scores"]
-                )
+    return globalsF.statistics()
 
 # 个人信息    
 @search.route('/profile', methods=['GET', 'POST'])
 def profile():
-    return render_template(
-        'profile.html',
-        status=globals.status,
-        id=globals.User["id"],
-        name=globals.User["name"],
-        gender=globals.User["gender"],
-        tele=globals.User["tele"],
-        brief=globals.User["brief"]
-        )
+    return globalsF.profile()
 
 # 登录界面    
 @search.route('/login', methods=['GET', 'POST'])
 def login():
-    globals.status = 1
-    globals.test = 0
-    globals.stat = 0
-    return render_template(
-        'profile.html',
-        status=globals.status,
-        error=globals.error,
-        retry=globals.retry
-        )
+    return globalsF.login()
 
 # 注册界面
 @search.route('/register', methods=['GET', 'POST'])
 def register():
-    globals.status=0
-    return render_template(
-        "profile.html",
-        status=globals.status,
-        error=globals.error,
-        retry=globals.retry
-        )
+    return globalsF.register()
 
 # 注册确认
 @search.route('/registration',methods=['GET', 'POST'])
 def registration():
-    globals.error=0
-    globals.retry=0
-    pw=request.args.get('pwReg')
-    pw2=request.args.get('pw2Reg')
-    if pw==pw2:
-        name=request.args.get("nameReg")
-        gender=request.args.get("genderReg")
-        tele=request.args.get("teleReg")
-        brief=request.args.get("briefReg")
-        if(name and gender and tele and brief and pw and pw2):
-            id=globals.ctrl.new_user(name,gender,tele,pw,brief)
-            globals.status=1
-            return render_template(
-                "validation.html",
-                status=globals.status,
-                id=id
-            )
-        else:
-            globals.retry=2
-            return render_template(
-                "profile.html",
-                status=globals.status,
-                retry=globals.retry
-            )
-    else:
-        globals.retry=1
-        return render_template(
-            "profile.html",
-            status=globals.status,
-            retry=globals.retry
-        )
+    return globalsF.registration()
 
 # 登录确认
 @search.route('/validation', methods=['GET', 'POST'])
 def validation():
-    globals.error=0
-    globals.retry=0
-    pw=str(request.form.get('pw'))
-    id=request.form.get('id')
-    temp=globals.ctrl.get_user_info_by_id(id)
-    try:
-        if temp[4]==pw:
-            globals.User={"id":temp[0],"name":temp[1],"gender":temp[2],"tele":temp[3],"pw":temp[4],"brief":temp[5]}
-            globals.status = 2
-            
-            if globals.test == 1:
-                globals.test = 0
-                return redirect(url_for('test'))
-            
-            elif globals.stat == 1:
-                globals.stat = 0
-                return redirect(url_for('statistics'))
-            
-            else:
-                return render_template(
-                    "index.html",
-                    status=globals.status,
-                    error=globals.error
-                    )
-        else:
-            globals.error=1
-            return render_template(
-                "profile.html",
-                status=globals.status,
-                error=globals.error
-                )
-    except:
-        globals.error=1
-        return render_template(
-            "profile.html",
-            status=globals.status,
-            error=globals.error
-            )
+    return globalsF.validation()
 
 # 下线
 @search.route("/logout",methods=['GET', 'POST'])
 def logout():
-    globals.status=1
-    return render_template(
-        "index.html",
-        status=globals.status
-    )  
+    return globalsF.logout()
+  
+def run():
+    search.run(debug=True)
     
-search.run(debug=True)
-    
-# def run_local():
-#     search.run()
+def run_local():
+    search.run()
 
-# def server_run():
-#     search.run(host='0.0.0.0', port=80)
+def server_run():
+    search.run(host='0.0.0.0', port=80)
 
-# if __name__ == '__main__':
-#     run()
+if __name__ == '__main__':
+    run()
